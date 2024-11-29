4dd813ebf868e5419f48793483c6eeade889aaa1
aaronke16 <124176003+aaronke16@users.noreply.github.com>
2024-11-29 01:16:28 -0500

    adding images and formatting the homepage correctly to align items to the middle

---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// import Instagram from "../../assets/svgs/brands/instagram.svg"
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+
+        <a href="https://www.instagram.com/aaronk.essien/" className="home__social-link" target="_blank">
+            <i class="" aria-hidden="true"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/aaron-essien/" className="home__social-link" target="_blank">
+            <i class="fa-brands fa-linkedin"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default HeaderSocials
