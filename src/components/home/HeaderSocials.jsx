c5af4b5c35c5ad27f2ba7775afa4d5ff79759763
aaronke16 <124176003+aaronke16@users.noreply.github.com>
2024-12-03 20:29:36 -0500

    removal of instagram icon

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -4,10 +4,6 @@ const HeaderSocials = () => {
   return (
     <div className="home__socials">
 
-        <a href="https://www.instagram.com/aaronk.essien/" className="home__social-link" target="_blank">
-            <i class="fa-brands fa-instagram" aria-hidden="true"></i>
-        </a>
-
         <a href="https://www.linkedin.com/in/aaronessien/" className="home__social-link" target="_blank">
             <i class="fa-brands fa-linkedin"></i>
         </a>
